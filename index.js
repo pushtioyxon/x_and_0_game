47a6e22477b101d87e01a0efa9e50f94516dcf41
pushtioyxon <pushtioyxon@gmail.com>
2025-10-28 22:42:22 +0500

    changed css js

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-const board = document.getElementById("board");
-let currentPlayer ="x"
-let gameBoard =["","","","","","","",""]
- function checkWinner(){
-    const winPatterns=[
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,4,8],
-        [2,4,6],
-    ]
- };
+const board = document.getElementById("board"); // bu bilan htmldagi div chaqirilyapti
+let currentPlayer = "X";
+let gameBoard = ["", "", "", "", "", "", "", "", ""]; // bu 9 ta katak shu x 0 uyiniga k bulaidgan
 
- for (let pattern of winPatterns){
-let [a,b,c]=pattern;
-if(
-    gameBoard[a]&&
-    gameBoard[a]== gameBoard[b]&&
-    gameBoard[a]==gameBoard[c]
-){
-    setTimeout(()=>alert(`${gameBoard[a]}yutdi`),100)
-    return true
-}return false
- }
- 
+function checkWinner() {
+  const winPatterns = [ // qaysilar bulsa yuta oladi
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
- function handleClick(index, cell) {
+  for (let pattern of winPatterns) {
+    let [a, b, c] = pattern;
+    if (
+      gameBoard[a] && // bu tuldirilganini aniqlash uchun
+      gameBoard[a] === gameBoard[b] && // birinchisi ikkinchisi  bir xil belgi borligini  uchun
+      gameBoard[a] === gameBoard[c]  // birinchisi uchinchisi bir xil belgi borligini  aniqlash uchun
+    ) {
+      setTimeout(() => alert(`${gameBoard[a]} yutdi!`), 100); //  alert kim yutganini kursatib beradi  settimeout esa birinchi usha oxirgi
+      //  tuldirilgan qator tuldirilsin keyin kim yutgani chiqarilsi bu yerda 100 quyilgan 100 ms dan kn golib chiqadi
+      return true;
+    }
+    
+  }
+  if (!gameBoard.includes("")) { // qachonki bush katak qolmasa
+    setTimeout(() => alert("Durrang!"), 100);  // 100 ms dan kn durrang chiqarsin
+    return true;
+  }
+  return false; // aks holda false
+}
+
+function handleClick(index, cell) {
   if (!gameBoard[index]) {
     gameBoard[index] = currentPlayer;
     cell.innerHTML =
